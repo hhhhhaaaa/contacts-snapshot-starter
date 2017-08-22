f2f6bbeeacdc182d024dc8fbf0ab5b8c189bda66
Doug L <douglubaway@gmail.com>
2017-08-22 14:14:22 -0700

    Establish express-session

--- src/server.js
+++ src/server.js
@@ -1,26 +1,45 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const logger = require('morgan');
+const session = require('express-session');
 const dbContacts = require('./db/contacts');
-const app = express();
 const {renderError} = require('./server/utils');
 const routes = require('./server/routes');
 
+const app = express();
+
+//View Engine
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+//Middleware
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use((request, response, next) => {
   response.locals.query = '';
   next();
 });
+app.use(logger('dev'));
+app.use(session({
+  key: 'user_id',
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { expires: 600000 }
+}));
+app.use(cookieParser());
 
+
+//Routes
 app.use('/', routes);
 
+//404
 app.use((request, response) => {
   response.render('not_found');
 });
 
+//Port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
